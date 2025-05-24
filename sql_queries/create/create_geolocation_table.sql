380ab3f867ae762af32c612d2f9b0b7cf85b5f2a
Alice Portes <144279042+aliceportes@users.noreply.github.com>
2025-05-24 02:15:27 -0300

    Add files via upload

---
+++ sql_queries/create/create_geolocation_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE geolocation (
+    geolocation_zip_code_prefix INTEGER,
+    geolocation_lat NUMERIC,
+    geolocation_lng NUMERIC,
+    geolocation_city TEXT,
+    geolocation_state TEXT
+);
