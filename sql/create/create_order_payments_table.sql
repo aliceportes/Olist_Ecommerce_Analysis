a57c6ba3db518735128a23691cd330d9584a12c7
Alice Portes <144279042+aliceportes@users.noreply.github.com>
2025-05-24 02:14:02 -0300

    Add files via upload

---
+++ sql/create/create_order_payments_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE order_payments (
+    order_id TEXT,
+    payment_sequential INTEGER,
+    payment_type TEXT,
+    payment_installments INTEGER,
+    payment_value NUMERIC
+);
---
+++ sql/create/create_order_reviews_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE order_reviews (
+    review_id TEXT,
+    order_id TEXT,
+    review_score INTEGER,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date TIMESTAMP,
+    review_answer_timestamp TIMESTAMP
+);
